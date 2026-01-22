4050088c4a031477e77ef7e1313485d7221ee98f
Sujin Lee <sooj2023@gmail.com>
2026-01-22 15:41:48 +0900

    style: switch to Pretendard font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 import type { Metadata } from "next";
-import { Inter, JetBrains_Mono } from "next/font/google";
+import localFont from "next/font/local";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
+const pretendard = localFont({
+  variable: "--font-pretendard",
+  src: [
+    { path: "../public/fonts/Pretendard-Thin.otf", weight: "100", style: "normal" },
+    { path: "../public/fonts/Pretendard-ExtraLight.otf", weight: "200", style: "normal" },
+    { path: "../public/fonts/Pretendard-Light.otf", weight: "300", style: "normal" },
+    { path: "../public/fonts/Pretendard-Regular.otf", weight: "400", style: "normal" },
+    { path: "../public/fonts/Pretendard-Medium.otf", weight: "500", style: "normal" },
+    { path: "../public/fonts/Pretendard-SemiBold.otf", weight: "600", style: "normal" },
+    { path: "../public/fonts/Pretendard-Bold.otf", weight: "700", style: "normal" },
+    { path: "../public/fonts/Pretendard-ExtraBold.otf", weight: "800", style: "normal" },
+  ],
 });
 
 const jetBrainsMono = JetBrains_Mono({
@@ -25,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${jetBrainsMono.variable} antialiased`}
+        className={`${pretendard.variable} ${jetBrainsMono.variable} antialiased`}
       >
         {children}
       </body>
